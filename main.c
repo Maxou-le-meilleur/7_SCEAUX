e5b8a1074e69309d8c7ced64f7e629814f730d81
Maxence <maxence.laune@estaca.eu>
2020-04-28 12:40:41 +0200

    j'ai pas mal avancé

--- main.c
+++ main.c
@@ -8,30 +8,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <time.h>
+
 
 // Déclaration des types structurés
-struct carte
+struct joueur
 {
-    int valeur;
-    char couleur[6];
+    char prenom[20];
+    int jeu[14];
 };
+typedef struct joueur S_joueur;
+S_joueur joueur[4];
 
 //Déclaration des prototypes
 int nombre_joueur(void);
-int distrib(n);
+int distrib(int n);
+int defdeck(int n);
 
 //Variable globales
 int n;
-char j1[20];
-char j2[20];
-char j3[20];
-char j4[20];
-char j5[20];
+int nb2cartes;
 
 //Programme principal
 
 int main()
 {
+    srand(time(NULL));
     printf("\n\n\n                                   Bonjour! Bienvenue au jeu des...\n\n\n");
     printf("                                               7 SEAUX\n\n\n");
     printf("                          Serez-vous le plus grand mage que l'histoire ait connu?\n\n\n\n");
@@ -42,256 +44,62 @@ int main()
 int nombre_joueur()
 {
     int n;
-    printf("                          Pour commencer veuillez indiquer le nombre de joueur : \n");
+    printf("                          Pour commencer veuillez indiquer le nombre de joueur : ");
     scanf("%d",&n);
-    printf("Le joueur ayant allume le jeu sera Ogarorin!\n");
-    Sleep (1000);
-    printf("Le joueur a la gauche de Ogarorin gauche sera Atras!\n");
-    Sleep (1000);
-    printf("Le joueur a la gauche de Astras sera Ekyorn!\n");
-    Sleep (1000);
+    printf("\nLe joueur ayant allume le jeu doit entrer son prenom : ");
+    scanf("%s",&joueur[0].prenom[20]);
+    Sleep (500);
+    printf("\nLe joueur a la gauche du joueur 2 doit entrer son prenom : ");
+    scanf("%s",&joueur[1].prenom[20]);
+    Sleep (500);
+    printf("\nLe joueur a la gauche du joueur 3 doit entrer son prenom : ");
+    scanf("%s",&joueur[2].prenom[20]);
+    Sleep (500);
     if (n==4)
     {
-        printf("Le joueur a la gauche de Ekyorn sera Vrilune!\n");
-        Sleep(1000);
+        printf("\nLe joueur a la gauche du joueur 4 doit entrer son prenom : ");
+        scanf("%s",&joueur[3].prenom[20]);
+        Sleep(500);
     }
     if (n==5)
     {
-        printf("Le joueur a la gauche de Vrilune sera Ezthar!\n");
+        printf("\nLe joueur a la gauche du joueur 5 doit entrer son prenom : ");
+        scanf("%s",&joueur[4].prenom[20]);
     }
     return n;
 }
 
-int distrib(n)
+int defdeck(int n)
 {
-    int cartes[75];
-    int i;
+    if (n==3)
+    {
+        nb2cartes = 45;
+    }
+    if (n==4)
+    {
+        nb2cartes = 60;
+    }
+    if (n==5)
+    {
+        nb2cartes = 75;
+    }
+    return nb2cartes;
+}
 
-        cartes[i].valeur = 1;
-        cartes[i].couleur = rouge;
-        i=i+1;
-        cartes[i].valeur = 2;
-        cartes[i].couleur = rouge;
-        i=i+1;
-        cartes[i].valeur = 3;
-        cartes[i].couleur = rouge;
-        i=i+1;
-        cartes[i].valeur = 4;
-        cartes[i].couleur = rouge;
-        i=i+1;
-        cartes[i].valeur = 5;
-        cartes[i].couleur = rouge;
-        i=i+1;
-        cartes[i].valeur = 6;
-        cartes[i].couleur = rouge;
-        i=i+1;
-        cartes[i].valeur = 7;
-        cartes[i].couleur = rouge;
-        i=i+1;
-        cartes[i].valeur = 8;
-        cartes[i].couleur = rouge;
-        i=i+1;
-        cartes[i].valeur = 9;
-        cartes[i].couleur = rouge;
-        i=i+1;
-        cartes[i].valeur = 1;
-        cartes[i].couleur = bleu;
-        i=i+1;
-        cartes[i].valeur = 2;
-        cartes[i].couleur = bleu;
-        i=i+1;
-        cartes[i].valeur = 3;
-        cartes[i].couleur = bleu;
-        i=i+1;
-        cartes[i].valeur = 4;
-        cartes[i].couleur = bleu;
-        i=i+1;
-        cartes[i].valeur = 5;
-        cartes[i].couleur = bleu;
-        i=i+1;
-        cartes[i].valeur = 6;
-        cartes[i].couleur = bleu;
-        i=i+1;
-        cartes[i].valeur = 7;
-        cartes[i].couleur = bleu;
-        i=i+1;
-        cartes[i].valeur = 8;
-        cartes[i].couleur = bleu;
-        i=i+1;
-        cartes[i].valeur = 9;
-        cartes[i].couleur = bleu;
-        i=i+1;
-        cartes[i].valeur = 1;
-        cartes[i].couleur = vert;
-        i=i+1;
-        cartes[i].valeur = 2;
-        cartes[i].couleur = vert;
-        i=i+1;
-        cartes[i].valeur = 3;
-        cartes[i].couleur = vert;
-        i=i+1;
-        cartes[i].valeur = 4;
-        cartes[i].couleur = vert;
-        i=i+1;
-        cartes[i].valeur = 5;
-        cartes[i].couleur = vert;
-        i=i+1;
-        cartes[i].valeur = 6;
-        cartes[i].couleur = vert;
-        i=i+1;
-        cartes[i].valeur = 7;
-        cartes[i].couleur = vert;
-        i=i+1;
-        cartes[i].valeur = 8;
-        cartes[i].couleur = vert;
-        i=i+1;
-        cartes[i].valeur = 9;
-        cartes[i].couleur = vert;
-        i=i+1;
-        cartes[i].valeur = 1;
-        cartes[i].couleur = jaune;
-        i=i+1;
-        cartes[i].valeur = 2;
-        cartes[i].couleur = jaune;
-        i=i+1;
-        cartes[i].valeur = 3;
-        cartes[i].couleur = jaune;
-        i=i+1;
-        cartes[i].valeur = 4;
-        cartes[i].couleur = jaune;
-        i=i+1;
-        cartes[i].valeur = 5;
-        cartes[i].couleur = jaune;
-        i=i+1;
-        cartes[i].valeur = 6;
-        cartes[i].couleur = jaune;
-        i=i+1;
-        cartes[i].valeur = 7;
-        cartes[i].couleur = jaune;
-        i=i+1;
-        cartes[i].valeur = 8;
-        cartes[i].couleur = jaune;
-        i=i+1;
-        cartes[i].valeur = 9;
-        cartes[i].couleur = jaune;
-        i=i+1;
-        cartes[i].valeur = 1;
-        cartes[i].couleur = violet;
-        i=i+1;
-        cartes[i].valeur = 2;
-        cartes[i].couleur = violet;
-        i=i+1;
-        cartes[i].valeur = 3;
-        cartes[i].couleur = violet;
-        i=i+1;
-        cartes[i].valeur = 4;
-        cartes[i].couleur = violet;
-        i=i+1;
-        cartes[i].valeur = 5;
-        cartes[i].couleur = violet;
-        i=i+1;
-        cartes[i].valeur = 6;
-        cartes[i].couleur = violet;
-        i=i+1;
-        cartes[i].valeur = 7;
-        cartes[i].couleur = violet;
-        i=i+1;
-        cartes[i].valeur = 8;
-        cartes[i].couleur = violet;
-        i=i+1;
-        cartes[i].valeur = 9;
-        cartes[i].couleur = violet;
-        i=i+1;
-        cartes[i].valeur = 10;
-        cartes[i].couleur = rouge;
-        i=i+1;
-        cartes[i].valeur = 11;
-        cartes[i].couleur = rouge;
-        i=i+1;
-        cartes[i].valeur = 12;
-        cartes[i].couleur = rouge;
-        i=i+1;
-        cartes[i].valeur = 10;
-        cartes[i].couleur = bleu;
-        i=i+1;
-        cartes[i].valeur = 11;
-        cartes[i].couleur = bleu;
-        i=i+1;
-        cartes[i].valeur = 12;
-        cartes[i].couleur = bleu;
-        i=i+1;
-        cartes[i].valeur = 10;
-        cartes[i].couleur = vert;
-        i=i+1;
-        cartes[i].valeur = 11;
-        cartes[i].couleur = vert;
-        i=i+1;
-        cartes[i].valeur = 12;
-        cartes[i].couleur = vert;
-        i=i+1;
-        cartes[i].valeur = 10;
-        cartes[i].couleur = jaune;
-        i=i+1;
-        cartes[i].valeur = 11;
-        cartes[i].couleur = jaune;
-        i=i+1;
-        cartes[i].valeur = 12;
-        cartes[i].couleur = jaune;
-        i=i+1;
-        cartes[i].valeur = 10;
-        cartes[i].couleur = violet;
-        i=i+1;
-        cartes[i].valeur = 11;
-        cartes[i].couleur = violet;
-        i=i+1;
-        cartes[i].valeur = 12;
-        cartes[i].couleur = violet;
-        i=i+1;
-        cartes[i].valeur = 13;
-        cartes[i].couleur = rouge;
-        i=i+1;
-        cartes[i].valeur = 14;
-        cartes[i].couleur = rouge;
-        i=i+1;
-        cartes[i].valeur = 15;
-        cartes[i].couleur = rouge;
-        i=i+1;
-        cartes[i].valeur = 13;
-        cartes[i].couleur = bleu;
-        i=i+1;
-        cartes[i].valeur = 14;
-        cartes[i].couleur = bleu;
-        i=i+1;
-        cartes[i].valeur = 15;
-        cartes[i].couleur = bleu;
-        i=i+1;
-        cartes[i].valeur = 13;
-        cartes[i].couleur = vert;
-        i=i+1;
-        cartes[i].valeur = 14;
-        cartes[i].couleur = vert;
-        i=i+1;
-        cartes[i].valeur = 15;
-        cartes[i].couleur = vert;
-        i=i+1;
-        cartes[i].valeur = 13;
-        cartes[i].couleur = jaune;
-        i=i+1;
-        cartes[i].valeur = 14;
-        cartes[i].couleur = jaune;
-        i=i+1;
-        cartes[i].valeur = 15;
-        cartes[i].couleur = jaune;
-        i=i+1;
-        cartes[i].valeur = 13;
-        cartes[i].couleur = violet;
-        i=i+1;
-        cartes[i].valeur = 14;
-        cartes[i].couleur = violet;
-        i=i+1;
-        cartes[i].valeur = 15;
-        cartes[i].couleur = violet;
-        i=i+1;
+int distrib(int n)
+{
+    int i;
+    int tab[nb2cartes - 1];
+    for (i=0;i<= nb2cartes - 1; i++)
+    {
+        tab[i] = i;
+    }
+    for (i=0;i<= nb2cartes - 1; i++)
+    {
+        int alea = rand()% (nb2cartes - 1);
+        joueur[i/15].jeu[i/n]=tab[alea];
+        tab[alea]=tab[nb2cartes - 1];
+    }
     return 0;
 }
 
